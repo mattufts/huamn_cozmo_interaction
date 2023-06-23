e0d84bc6d46a251700ab631b74e0ce459817f1fa
Matthew <matthew.ebisu@tufts.edu>
2023-06-23 16:14:49 -0400

    updated with direction to cozmo_controller
    Added asyncio.run(main()) at the end: The main entry point of the program is wrapped in the main() function, which is then run using asyncio.run(). This ensures that the event loop is properly managed and executed.
    Created a main() function: This function serves as the main entry point for the program. It creates a Cozmo robot object using await cozmo.robot.Robot.wait_for_robot() and then calls the act() function.
    Removed cozmo.run_program(): The cozmo.run_program() function was causing the issue because it tries to create its own event loop, which conflicts with the existing event loop. By removing it, we ensure that the code runs within the same event loop context.
    Reorganized code structure: The code has been rearranged to follow a more standard structure. Global variables, functions, and the main entry point have been organized and indented properly for better readability.
    Overall, these changes ensure that the event loop is managed c
    
    thanks chatgpt..

--- Emotion_Eyes/Emoticons/cozmo_controller_emoticons.py
+++ Emotion_Eyes/Emoticons/cozmo_controller_emoticons.py
@@ -15,7 +15,7 @@ import asyncio
 import time
 from cozmo.util import degrees, distance_mm, speed_mmps
 
-try: 
+try:
     from PIL import Image
 except ImportError:
     sys.exit("Cannot import from PIL: Do `pip3 install --user Pillow` to install")
@@ -23,80 +23,88 @@ except ImportError:
 angle = 0
 distance = 80
 speed = 50
-expression = None 
-front = None    #represents the global variable for identifying front of Cozmo
-duration = 2000 #duration is how long the animation stays on cozmo's face
+expression = None
+front = None  # represents the global variable for identifying front of Cozmo
+duration = 2000  # duration is how long the animation stays on cozmo's face
 
 async def turn_angle(robot: cozmo.robot.Robot, angle: float):
     await robot.turn_in_place(degrees(angle)).wait_for_completed()
 
 async def move_forward(robot: cozmo.robot.Robot, distance: float, speed: float):
     await robot.drive_straight(distance_mm(distance), speed_mmps(speed), should_play_anim=False, in_parallel=True).wait_for_completed()
-    
-async def act(robot: cozmo.robot.Robot): 
+
+async def act(robot: cozmo.robot.Robot):
     # Turn Cozmo by a specific angle (in degrees)
     angle_to_turn = angle  # Set the angle you want to turn (in degrees)
     await turn_angle(robot, angle_to_turn)
-    #Call cozmo_show_img function to display the image
-    await cozmo_show_img(robot) #call the function to display the image
+    # Call the cozmo_show_img function to display the image
+    await cozmo_show_img(robot)
     # Move Cozmo forward by a specific distance (in millimeters)
     distance_to_move = distance  # Set the distance you want to move (in millimeters)
     speed_to_move = speed  # Set the speed you want to move (in millimeters per second)
     print("Turning Angle: ", angle_to_turn)
     await move_forward(robot, distance_to_move, speed_to_move)
 
-#This function displays an animation on cozmo's face based on the environment
+# This function displays an image on Cozmo's face based on the environment
 async def cozmo_show_img(robot: cozmo.robot.Robot):
-    #set the default image to neutral
-    default_image ="neutral.png" 
+    # Set the default image to neutral
+    default_image = "neutral.png"
 
-# change the expression based on what is front    
+    # Change the expression based on what is in front
     img = None
-    if front == "wall":   
-        img = "Angry_Stop-01.png"
-        cozmo.run_program(act).wait_for_completed()
-    if front == "nothing":
-        img = "neutral.png"  
-        cozmo.run_program(act).wait_for_completed()
-    if front == "goal":
-        img = "happy-01.png"
-        cozmo.run_program(act).wait_for_completed()
-    if front == "hit":
-        img = "sudden_hit-01.png"
-        cozmo.run_program(act).wait_for_completed()
-    if front == "left":
-        img = "glancing_left-01.png"
-        cozmo.run_program(act).wait_for_completed()
-    if front == "right":
-        img = "glancing_right-01.png"
-        cozmo.run_program(act).wait_for_completed()
-
-# Use the default image if no other image is determined
-    if img is None: 
-        img = default_image
-        image = Image.open(img)
-        resized_image = image.resize(cozmo.oled_face.dimensions(), Image.BICUBIC)
-        face_image = cozmo.oled_face.convert_image_to_screen_data(resized_image, invert_image=True)
-        robot.display_oled_face_image(face_image, duration)
-
-    if img is not None:
-        image = Image.open(img)
-        resized_image = image.resize(cozmo.oled_face.dimensions(), Image.BICUBIC)
-        face_image = cozmo.oled_face.convert_image_to_screen_data(resized_image, invert_image=True)
-        robot.display_oled_face_image(face_image, duration)
-        
-async def cozmo_show_animation(robot: cozmo.robot.Robot):
-    img = []
     if front == "wall":
-        path = "/home/jstaley/hang_yu/huamn_cozmo_interaction/Emotion_Eyes/Blinking/eyes_blink"
-        for i in range(1, 7):
-            img.append(path + str(i) + ".png")
-    if front == "nothing":
-        pass
-    if len(img) > 0:
-        for i in range(0, len(img)):
-            image = Image.open(img[i])
-            resized_image = image.resize(cozmo.oled_face.dimensions(), Image.BICUBIC)
-            face_image = cozmo.oled_face.convert_image_to_screen_data(resized_image, invert_image=True)
-            robot.display_oled_face_image(face_image, duration/2)
-            await asyncio.sleep(0.2)
+        img = "Angry_Stop-01.png"
+    elif front == "nothing":
+        img = "neutral.png"
+    elif front == "goal":
+        img = "happy-01.png"
+    elif front == "hit":
+        img = "sudden_hit-01.png"
+    elif front == "left":
+        img = "glancing_left-01.png"
+    elif front == "right":
+        img = "glancing_right-01.png"
+
+    # Use the default image if no other image is determined
+    if img is None:
+        img = default_image
+
+    image = Image.open(img)
+    resized_image = image.resize(cozmo.oled_face.dimensions(), Image.BICUBIC)
+    face_image = cozmo.oled_face.convert_image_to_screen_data(resized_image, invert_image=True)
+    robot.display_oled_face_image(face_image, duration)
+
+async def main():
+    # Create a Cozmo robot object
+    robot = await cozmo.robot.Robot.wait_for_robot()
+
+    # Call the act function
+    await act(robot)
+
+if __name__ == '__main__':
+    # Run the main function within a new event loop
+    asyncio.run(main())
+
+    
+    # if img is not None:
+    #     image = Image.open(img)
+    #     resized_image = image.resize(cozmo.oled_face.dimensions(), Image.BICUBIC)
+    #     face_image = cozmo.oled_face.convert_image_to_screen_data(resized_image, invert_image=True)
+    #     robot.display_oled_face_image(face_image, duration)
+  
+      
+# async def cozmo_show_animation(robot: cozmo.robot.Robot):
+#     img = []
+#     if front == "wall":
+#         path = "/home/jstaley/hang_yu/huamn_cozmo_interaction/Emotion_Eyes/Blinking/eyes_blink"
+#         for i in range(1, 7):
+#             img.append(path + str(i) + ".png")
+#     if front == "nothing":
+#         pass
+#     if len(img) > 0:
+#         for i in range(0, len(img)):
+#             image = Image.open(img[i])
+#             resized_image = image.resize(cozmo.oled_face.dimensions(), Image.BICUBIC)
+#             face_image = cozmo.oled_face.convert_image_to_screen_data(resized_image, invert_image=True)
+#             robot.display_oled_face_image(face_image, duration/2)
+#             await asyncio.sleep(0.2)
