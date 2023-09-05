d0224d0c97373556bfec881ab1d9de85bf89c3cd
Matthew <matthew.ebisu@tufts.edu>
2023-09-04 22:54:14 -0400

    updates to pycozmoFSM script

--- Emotion_Eyes/Emoticons/PycozmoFSM.py
+++ Emotion_Eyes/Emoticons/PycozmoFSM.py
@@ -2,89 +2,10 @@
 #and is a rework of the AsyncFSM.py script 
  
 import sys
-import cozmo
-import asyncio
-from cozmo.util import degrees, distance_mm, speed_mmps
-
-try:
-    from PIL import Image
-except ImportError:
-    sys.exit("Cannot import from PIL: Do `pip3 install --user Pillow` to install")
-
-angle = 0
-distance = 80
-speed = 50
-expression = None
-front = None  # represents the global variable for identifying front of Cozmo
-duration = 2000  # duration is how long the animation stays on cozmo's face
-
-async def turn_angle(robot: cozmo.robot.Robot, angle: float):
-    await robot.turn_in_place(degrees(angle)).wait_for_completed()
-
-async def move_forward(robot: cozmo.robot.Robot, distance: float, speed: float):
-    await robot.drive_straight(distance_mm(distance), speed_mmps(speed), should_play_anim=False, in_parallel=True).wait_for_completed()
-
-async def act(robot: cozmo.robot.Robot):
-    # Turn Cozmo by a specific angle (in degrees)
-    angle_to_turn = angle  # Set the angle you want to turn (in degrees)
-    await turn_angle(robot, angle_to_turn)
-    # Call the cozmo_show_img function to display the image
-    await cozmo_show_img(robot)
-    # Move Cozmo forward by a specific distance (in millimeters)
-    distance_to_move = distance  # Set the distance you want to move (in millimeters)
-    speed_to_move = speed  # Set the speed you want to move (in millimeters per second)
-    print("Turning Angle: ", angle_to_turn)
-    await move_forward(robot, distance_to_move, speed_to_move)
-
-# This function displays an image on Cozmo's face based on the environment
-async def cozmo_show_img(robot: cozmo.robot.Robot):
-    # Set the default image to neutral
-    default_image = "neutral.png"
-
-    # Change the expression based on what is in front
-    img = None
-    if front == "wall":
-        img = "angry.png"  # Set the appropriate image for this state
-    elif front == "nothing":
-        img = "neutral.png"
-    elif front == "goal":
-        img = "happy.png"  # Set the appropriate image for this state
-    elif front == "hit":
-        img = "angry.png"  # Set the appropriate image for this state
-    elif front == "left":
-        img = "neutral.png"
-    elif front == "right":
-        img = "neutral.png"
-
-    # Use the default image if no other image is determined
-    if img is None:
-        img = default_image
-
-    image = Image.open(img)
-    resized_image = image.resize(cozmo.oled_face.dimensions(), Image.BICUBIC)
-    face_image = cozmo.oled_face.convert_image_to_screen_data(resized_image, invert_image=True)
-    robot.display_oled_face_image(face_image, duration)
-
-     # Set the idle animation during image display
-    robot.set_idle_animation(Triggers.Neutral)
-
-async def main():
-    # Create a Cozmo robot object
-    robot = await cozmo.robot.Robot.wait_for_robot()
-
-    # Call the act function
-    await act(robot)
-
-if __name__ == '__main__':
-    # Run the main function within a new event loop
-    asyncio.run(main())
-
-import sys
 import os
 import pycozmo
-import time
-from pycozmo import util   
-from pycozmo.util import Angle, Distance, Speed, Pose, Angle, Vector3, Pose, Quaternion
+import time 
+from pycozmo.util import Angle, Distance, Speed, Pose, Quaternion
 from pycozmo import protocol_encoder
 
 try:
@@ -93,10 +14,9 @@ except ImportError:
     sys.exit("Cannot import from PIL: Do `pip3 install --user Pillow` to install")
 
 # Define global variables
-Angle = 0
+Angle = 
 Distance = 80
 Speed = 50
-
 front = None
 duration = 2000
 default_image = "blank.png"
@@ -156,12 +76,12 @@ def run_fsm(robot: pycozmo.client):
 def main():
     with pycozmo.connect() as cli:
         # # Connect to the Cozmo robot
-        #cli.start()
-        cli.connect()
+        head_angle = (pycozmo.MAX_HEAD_ANGLE.radians - pycozmo.robot.MIN_HEAD_ANGLE.radians)/2.0
+        cli.set_head_angle(head_angle)
         run_fsm(pycozmo.robot) 
         cli.wait_for_robot()
-    #cli.disconnect()
-    #cli.stop()
+    cli.disconnect()
+    cli.stop()
 
 if __name__ == '__main__':
     main()
--- Emotion_Eyes/Emoticons/pycozmo_ref_scripts/Pycozmo_README.py
+++ Emotion_Eyes/Emoticons/pycozmo_ref_scripts/Pycozmo_README.py
@@ -23,9 +23,43 @@ with pycozmo.connect() as cli:
 
 
 #how to get the head to tilt in a specific angle
-angle = (pycozmo.robot.MAX_HEAD_ANGLE.radians - pycozmo.robot.MIN_HEAD_ANGLE.radians) / 2.0
-cli.set_head_angle(angle) 
+head_angle = (pycozmo.robot.MAX_HEAD_ANGLE.radians - pycozmo.robot.MIN_HEAD_ANGLE.radians) / 2.0
+cli.set_head_angle(head_angle) 
+time.sleep(1) #may be optional
+
+
+#how to make cozmo go to a specific pose
+#this is used in go_to_pose.py script
+#it utilizes the pycozmo.util object
+with pycozmo.connect() as cli:
+	target = pycozmo.util.Pose(200, 100.0, 0.0, angle_z =pycozmo.util.Angle(degrees=0.0))
+	cli.go_to_pose(target, relative_to_robot=True)
 
 
 #how to define paramters for angle, distance, speed, expression, and duration
+#this is made use of int the path.py script
+SPEED_MMPS = 100.0 
+ACCEL_MMPS2 = 20.0
+DECEL_MMPS2 = 20.0 #keep these as global variables
+
+e = Event()  #this is part of the threading function 
+
+def on_path_following(......)
+	#this is for event handling using the 'threading' library
+
+with pycozmo.connect() as cli:
+		cli.add_handler(pycozmo.protocol_encoder.PathFollowingEvent, on_path_following)  #event handling for following events
+		cli.add_handler(pycozmo.EvtRobotPathingChang, on_robot_pathing_change) #event handling print statements
+		
+		#use the pycozmo object protocol_encoder and AppendPathSegLine to 
+		#obtain the values for from_x, from_y and to_x and to_y for where the 
+		#robot will be travelling
+		pkt = pycozmo.protocol_encoder.AppendPathSegLine(
+			from_x = 0.0, from_y = 0.0
+			to_x = 150.0, to_y = 0.0 
+			speed_mmps=SPEED_MMPS, accel_mmps2=ACCEL_MMPS2, decel_mmps2 = DECEL_MMPS2)
+		cli.conn.send(pkt) #send and update the variables using the protocol_encoder and pkt object
+
+
+
 
