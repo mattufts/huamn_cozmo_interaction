4906679630c38c5e2f635763b615eb4260ee65d4
Matthew <matthew.ebisu@tufts.edu>
2023-09-13 12:03:45 -0400

    Updated scripts

---
+++ Pycozmo Scripts/PycozmoFSM.py
@@ -0,0 +1,94 @@
+#This script needs to incorporate the pycozmo library
+#and is a rework of the AsyncFSM.py script 
+ 
+import sys
+import os
+import pycozmo
+import time 
+
+from pycozmo.util import Angle, Distance, Speed, Pose, Angle
+from pycozmo import protocol_encoder
+
+try:
+    from PIL import Image
+except ImportError:
+    sys.exit("Cannot import from PIL: Do `pip3 install --user Pillow` to install")
+
+# Define global variables
+Angle = 0
+Distance = 80
+Speed = 50
+front = None
+duration = 2000
+default_image = "blank.png"
+
+cli = pycozmo.Client()
+
+# FSM state functions
+def act(robot: pycozmo.client):
+    angle_to_turn = Angle
+    turn_angle(robot, angle_to_turn)
+    distance_to_move = Distance
+    speed_to_move = Speed
+    print("Turning Angle: ", angle_to_turn)
+    move_forward(robot, distance_to_move, speed_to_move)
+    show_image(robot, default_image)
+    
+def explore_state(robot: pycozmo.client):
+    print("Exploring...")
+    angle_to_turn = Angle
+    turn_angle(angle_to_turn)
+    distance_to_move = Distance
+    speed_to_move = Speed
+    print("Turning Angle: ", angle_to_turn)
+    move_forward(robot, distance_to_move, speed_to_move)
+    return interact_state
+
+def interact_state(robot: pycozmo.client):
+    #print("Interacting...")
+    #time.sleep(3)
+    return explore_state
+
+# Helper functions
+def turn_angle(Angle: float):
+    protocol_encoder.TurnInPlace(Angle)
+
+def move_forward(robot: pycozmo.client, Distance: float, Speed: float):
+    cli.drive_wheels (lwheel_speed=50.0, rwheel_speed=50.0, duration=2.0)
+    print ("Driving Straight")
+    #robot.drive_straight(distance_mm=Distance, speed_mmps=Speed, should_play_anim=False).wait_for_completed()
+
+def show_image(robot: pycozmo.client, image_path: str):
+    #image = Image.open()
+    target_size = (128, 32)
+    image = Image.open(os.path.join(image_path))
+    resized_image = image.resize(target_size, Image.ANTIALIAS)
+    image =  resized_image.convert('1')
+    cli.display_image(image)
+
+# Define the FSM execution function
+def run_fsm(robot: pycozmo.client):
+    current_state = explore_state
+    show_image(robot, default_image)
+    while True:
+        current_state = current_state(robot)
+
+# Define the main function
+# The main function should connect to cozmo immediately and start the FSM
+def main():
+    with pycozmo.connect() as pyc:
+        cli.start()
+        cli.connect()
+        # # Connect to the Cozmo robot
+        #turn off "alive" animations for cozmo
+        cli.anim_controller.enable_animations(False)
+        head_angle = (pycozmo.MAX_HEAD_ANGLE.radians - pycozmo.robot.MIN_HEAD_ANGLE.radians)/2.0
+        
+        pyc.set_head_angle(head_angle)
+        run_fsm(pycozmo.robot) 
+        pyc.wait_for_robot()
+    pyc.disconnect()
+    pyc.stop()
+
+if __name__ == '__main__':
+    main()
---
+++ Pycozmo Scripts/pycozmo_drive_straight.py
@@ -0,0 +1,66 @@
+# from threading import Event
+
+# import pycozmo
+
+# SPEED_MMPS = 100.0
+# ACCEL_MMPS2 = 20.0
+# DECEL_MMPS2 = 20.0
+
+# e = Event()
+
+# #on_path_following_event is for the robot to stop after it has completed the path
+# def on_path_following_event(cli, pkt: pycozmo.protocol_encoder.PathFollowingEvent):
+#     print(pkt.event_type)
+#     # if the packet is not a path started event, set the event
+#     if pkt.event_type != pycozmo.protocol_encoder.PathEventType.PATH_STARTED:
+#         #set the event
+#         e.set()
+
+# def on_robot_pathing_change(cli, state: bool):
+#     if state:
+#         print("Started pathing.")
+#     else:
+#         print("Stopped pathing.")
+
+
+# #connect to the robot
+# with pycozmo.connect() as cli:
+#     #add the event handlers
+#     cli.add_handler(pycozmo.protocol_encoder.PathFollowingEvent, on_path_following_event)
+#     #add the event handler to the robot pathing change event
+#     cli.add_handler(pycozmo.event.EvtRobotPathingChange, on_robot_pathing_change)
+
+#     #pkt is for the robot to drive straight
+#     pkt = pycozmo.protocol_encoder.AppendPathSegLine(
+#         #from_x and from_y are the starting coordinates
+#         from_x=0.0, from_y=0.0,
+#         #to_x and to_y are the ending coordinates
+#         to_x=150.0, to_y=0.0,
+#         #speed_mmps is the speed of the robot in millimeters per second
+#         speed_mmps=SPEED_MMPS, accel_mmps2=ACCEL_MMPS2, decel_mmps2=DECEL_MMPS2)
+#     #send the packet
+#     cli.conn.send(pkt)
+#     #pkt is for the robot to execute the path
+#     pkt = pycozmo.protocol_encoder.ExecutePath(event_id=1)
+#     cli.conn.send(pkt)
+
+#     e.wait(timeout=30.0)
+
+
+#simplified drive forward script
+import pycozmo
+cli = pycozmo.Client()
+cli.start()
+cli.connect()
+cli.wait_for_robot()
+
+#Set the animations to False for turning off the background animations
+#doing this will turn the animations off as the robot moves
+cli.anim_controller.enable_animations(False)
+
+#cli.anim_controller.cancel_anim()
+# pycozmo.anim_controller.enable_animations()
+# pycozmo.anim_controller.stop()
+cli.drive_wheels(lwheel_speed=50.0, rwheel_speed=50.0, duration=2.0)
+cli.disconnect()
+cli.stop()
