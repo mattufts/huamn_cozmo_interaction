f81e37bd261fe507fd08fadfe8c0dface9055095
Matthew <matthew.ebisu@tufts.edu>
2024-03-09 12:57:53 -0500

    updates to script

--- Pycozmo Scripts/AnimImages/animation_test_II.py
+++ Pycozmo Scripts/AnimImages/animation_test_II.py
@@ -15,11 +15,11 @@ def display_resized_image(cli, image_path):
         img = image_inverted.convert('1')
 
         cli.display_image(img)
-        #time.sleep(0.01) #the time here dictates whether cozmo's face will blend into other animations
+        time.sleep(0.01) #the time here dictates whether cozmo's face will blend into other animations
     else:
         print(f"Image file not found: {image_path}")
 
-def display_images(cli, base_path, fps=10):
+def display_images(cli, base_path, fps=24):
     #frame_duration = 1.0 / fps
 
     image_files = sorted([f for f in os.listdir(base_path) if f.endswith('.png')])
@@ -37,9 +37,9 @@ def main():
         time.sleep(2)
      
 
-        base_path = "/Users/matt/Documents/GitHub/human_cozmo_interaction/Pycozmo Scripts/AnimImages/Left"
+        base_path = "/Users/matt/Documents/GitHub/human_cozmo_interaction/Pycozmo Scripts/AnimImages/Right"
         display_images(cli, base_path)
-
+    
 
 if __name__ == '__main__':
     main()
